a11d55eb7c4d6707f9f00ef2742aafdee3f66a73
Michael Fournier <michaelfournier064@outlook.com>
2025-09-15 16:41:22 -0400

    feat(ui): title-screen - add hover/press sounds via ButtonUtil

---
+++ scripts/title_screen.gd
@@ -0,0 +1,18 @@
+# res://scripts/title_screen.gd
+extends Node2D
+
+@onready var quit_button:      CustomButton = $AspectRatioContainer/VBoxContainer/QuitGameButton
+@onready var new_game_button:  CustomButton = $AspectRatioContainer/VBoxContainer/NewGameButton
+@onready var load_game_button: CustomButton = $AspectRatioContainer/VBoxContainer/LoadGameButton
+@onready var settings_button:  CustomButton = $AspectRatioContainer/VBoxContainer/SettingsButton
+
+func _ready() -> void:
+	quit_button.pressed_confirmed.connect(_on_quit_game_pressed)
+	new_game_button.pressed_confirmed.connect(on_new_game_pressed)
+	load_game_button.pressed_confirmed.connect(on_load_game_pressed)
+	settings_button.pressed_confirmed.connect(on_settings_pressed)
+
+func on_new_game_pressed() -> void: print("New Game Pressed")
+func on_load_game_pressed() -> void: print("Load Game Pressed")
+func on_settings_pressed() -> void: print("Settings Pressed")
+func _on_quit_game_pressed() -> void: get_tree().quit()
